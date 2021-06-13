84b4792cd1c0b2c6f0ff2159b2909708c1b24fc6
Stefan Alexandru Ghinescu <stefan.alexandru.ghinescu@cern.ch>
2021-06-13 17:56:57 +0300

    Part 2. Forgot sources.

---
+++ src/config_utility.cc
@@ -0,0 +1,161 @@
+#include "config_utility.hh"
+
+#include <fstream>
+#include <iostream>
+#include <stdlib.h>
+#include <algorithm>
+#include <sstream>
+#include <vector>
+#include <math.h>
+
+config_utility *config_utility::fInstance = 0;
+
+config_utility *config_utility::GetInstance(){
+    if (!fInstance) fInstance = new config_utility();
+    return fInstance;
+}
+
+config_utility::config_utility(){
+
+}
+
+config_utility::~config_utility(){
+
+}
+
+void config_utility::Initialize(const std::string &configFileName){
+    std::ifstream configFile(configFileName.data());
+    if (!configFile.is_open()){
+        std::cerr << "Could not open config file: " << configFileName.data() << "\n";
+    }
+    std::cout << "Reading from configuration file..." << std::endl;
+
+    std::string line;
+    while(getline(configFile, line)){
+
+        // if line starts with "#", it's a comment. Ignore
+        if (line.empty() || line[0] == '#')
+            continue;
+        std::vector<std::string> tokens;
+
+        std::stringstream tmp(line);
+        std::string interm;
+
+        // Tokenize w.r.t. ' '
+        while (std::getline(tmp, interm, ' '))
+            tokens.push_back(interm);
+
+        // now really read the configuration
+        if (tokens[0].find("processName") != std::string::npos){
+            processName = tokens[1];
+            tokens.clear();
+
+            std::cout << "Process Name = " << fInstance->processName.data() << std::endl;
+            continue;
+        }
+        if (tokens[0].find("nucleusName") != std::string::npos){
+            nucleusName = tokens[1];
+            tokens.clear();
+            std::cout << "Nucleus Name = " << fInstance->nucleusName.data() << std::endl;
+            continue;
+        }
+        if (tokens[0].find("zParent") != std::string::npos){
+            zParent = std::atoi(tokens[1].data());
+            tokens.clear();
+            continue;
+        }
+        if (tokens[0].find("aParent") != std::string::npos){
+            aParent = std::atoi(tokens[1].data());
+            tokens.clear();
+            continue;
+        }
+        if (tokens[0].find("radiusBounds") != std::string::npos){
+            minimumRadius = std::atof(tokens[1].data());
+            maximumRadius = std::atof(tokens[2].data());
+            tokens.clear();
+            std::cout << "Radius bounds for computations: min " << fInstance->minimumRadius << " fm" <<
+            " max " << fInstance->maximumRadius << " fm" <<std::endl;
+            continue;
+        }
+        if (tokens[0].find("nRadialPoints") != std::string::npos){
+            nRadialPoints = std::atoi(tokens[1].data());
+            tokens.clear();
+            std::cout << "Number of radial points = " << fInstance->nRadialPoints << std::endl;
+            continue;
+        }
+        if (tokens[0].find("wavefunctionsType") != std::string::npos){
+            wfType = tokens[1];
+            tokens.clear();
+            std::cout << "Will compute " << fInstance->wfType.data() << " wavefunctions" <<std::endl;
+            continue;
+        }
+        if (tokens[0].find("potentialType") != std::string::npos){
+            potentialType = tokens[1];
+            tokens.clear();
+            std::cout << "Will use " << fInstance->potentialType.data() << " potential" <<std::endl;
+            continue;
+        }
+        if (tokens[0].find("applyScreening") != std::string::npos){
+            applyScreening = std::atoi(tokens[1].data());
+            tokens.clear();
+            std::cout << "Will ";
+            if (applyScreening)
+                std::cout << "";
+            else std::cout << "NOT ";
+            std::cout << "apply screening correction" <<std::endl;
+            continue;
+        }
+        if (tokens[0].find("maximalEnergy") != std::string::npos){
+            maximalEnergy = std::atof(tokens[1].data());
+            tokens.clear();
+            continue;
+        }
+        if (tokens[0].find("nEnergyPoints") != std::string::npos){
+            nEnergyPoints = std::atoi(tokens[1].data());
+            tokens.clear();
+            continue;
+        }
+        if (tokens[0].find("writeWF") != std::string::npos){
+            writeWF = std::atoi(tokens[1].data());
+            tokens.clear();
+            continue;
+        }
+        if (tokens[0].find("kBounds") != std::string::npos){
+            kBounds[0] = std::atoi(tokens[1].data());
+            kBounds[1] = std::atoi(tokens[2].data());
+            tokens.clear();
+            std::cout << "Will compute for k between " << fInstance->kBounds[0] <<
+             " and " << fInstance->kBounds[1] << std::endl;
+            continue;
+        }
+        if (tokens[0].find("maxPrincipalQN") != std::string::npos){
+            maxPrincipalQN = std::atoi(tokens[1].data());
+            tokens.clear();
+            continue;
+        }
+
+    }
+
+    std::cout << "Parent nucleus: Z = " << zParent <<
+        " A = " << aParent << std::endl;
+    if (processName.find("beta") != std::string::npos){
+        if (wfType.find("bound") != std::string::npos){
+            std::cerr << "ERROR! Wrong configuration. Bound states only for EC" << std::endl;
+            exit(1);
+        }
+        std::cout << "Maximal energy = " << maximalEnergy << " MeV" << std::endl;
+        std::cout << "Number of energy points = " << nEnergyPoints << std::endl;
+    }
+    if (writeWF){
+        if (wfType.find("beta") != std::string::npos){
+            std::cout << "Will write radial wave-functions at each energy point" << std::endl;
+        }
+    }
+    if (wfType.find("bound") != std::string::npos){
+        std::cout << "Will compute for n up to " << maxPrincipalQN << std::endl;
+    }
+    configFile.close();
+    std::cout << "... done reading configuration" << std::endl << std::endl;
+
+    nuclearRadius = 1.2 * std::pow(aParent, 1./3.);
+}
---
+++ src/math_tools.cc
@@ -0,0 +1,168 @@
+#include "math_tools.hh"
+#include "physical_constants.hh"
+
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <map>
+#include <math.h>
+
+using namespace physical_constants;
+math_tools *math_tools::fInstance = 0;
+
+math_tools *math_tools::GetInstance(){
+    if (!fInstance) fInstance = new math_tools();
+    return fInstance;
+}
+
+math_tools::math_tools():
+screenXvals(0),
+screenPhiVals(0),
+glNodes(0),
+glWeights(0)
+{
+}
+
+math_tools::~math_tools(){
+    glNodes.clear();
+    glWeights.clear();
+}
+
+void math_tools::ComputeEnergyPoints(int nPoints, double maxValue, std::vector<double> &energyPoints){
+    if (energyPoints.size() != 0) energyPoints.clear();
+    energyPoints.resize(nPoints);
+
+    double eMaxLog = std::log10(maxValue);
+    double eMinLog = -6.0;
+    double de = (eMaxLog - eMinLog)/(nPoints-1.);
+
+    for (int i = 0; i < nPoints; i++){
+        double elog = eMinLog + i*de;
+        energyPoints[i] = std::pow(10., elog);
+    }
+}
+
+void math_tools::GLNodesAndWeights(int nPoints){
+    std::vector<double> fx(nPoints+1);
+    std::vector<double> f1x(nPoints);
+    double fpx;
+
+    glNodes.clear();
+    glWeights.clear();
+
+    glNodes.resize(nPoints);
+    glWeights.resize(nPoints);
+    for (int i = 0; i < nPoints; i++){
+        glNodes[i] = std::cos(pi*(i+1.0-0.25)/(nPoints + 0.5));
+        for (int j = 0; j < 20; j++){
+            Legangl(nPoints+1, glNodes[i], fx);
+            Legangl(nPoints, glNodes[i], f1x);
+            fpx = nPoints*(glNodes[i]*fx[nPoints] - f1x[nPoints-1])/(std::pow(glNodes[i],2) -1.);
+            glNodes[i] = glNodes[i] - fx[nPoints]/fpx;
+            if (abs(fx[nPoints]) < 1E-12)
+                break;
+        }
+        glWeights[i] = 2./((1.-glNodes[i]*glNodes[i])*fpx*fpx);
+    }
+}
+
+void math_tools::Legangl(int n, double x, std::vector<double> &fx){
+    fx[0] = 1.;
+    fx[1] = x;
+    for (int i = 1; i < n; i++){
+        fx[i+1] = ((i*2. + 1.)*x*fx[i]-i*fx[i-1])/(i+1.);
+    }
+}
+
+double math_tools::ChargedSpherePot(double z, double rSphere, double r){
+    if (r <= rSphere){
+        return z * fineStructure * hc/(2.*rSphere) *
+            (3. - std::pow(r/rSphere, 2.));
+    }
+    else {
+        return z*fineStructure *hc/r;
+    }
+
+    return 0.;
+}
+
+void math_tools::ComputeScreeningFunction(int nPoints, double xMax, int nMaxCoeff, bool dump){
+    std::vector<double> expansCoeffs(nMaxCoeff);
+    ComputeExpansionCoefficients(expansCoeffs);
+
+    std::vector<double> xGL(31);
+    std::vector<double> wGL(31);
+    GLNodesAndWeights(31);
+
+
+    for (int i = 0; i < 31; i++){
+        xGL[i] = glNodes[31-i-1];
+        wGL[i] = glWeights[31-i-1];
+    }
+
+    std::ofstream screeningFile;
+    if (dump)
+        screeningFile.open("screening_test.dat", std::ofstream::trunc);
+
+    bool isLast = 0;
+    for (int i = 0; i < nPoints; i++){
+        double tMax = i/(nPoints -1.);
+
+        double sum = 0.;
+        double tVals[31];
+        double uVals[31];
+        double wVals[31];
+        for (int j = 0; j < 31; j++){
+            tVals[j] = 0.5 * tMax * xGL[j] + 0.5 * tMax;
+
+            uVals[j] = 0.0;
+            for (int m = 0; m < nMaxCoeff; m++){
+                uVals[j] += expansCoeffs[m]*std::pow(1.-tVals[j], m);
+            }
+
+            wVals[j] = -6.*uVals[j]*tVals[j]/(1.-tVals[j]*tVals[j]*uVals[j]);
+            sum += 0.5 * tMax * wGL[j]*wVals[j];
+        }
+
+        double x = std::pow(144., 1./3.)*tMax*tMax * std::exp(-sum/3.);
+        if (x > xMax) isLast = true;
+
+        double phi = std::exp(sum);
+        if (phi < 1E-15) phi = 0.;
+
+        screenXvals.push_back(x);
+        screenPhiVals.push_back(phi);
+
+        if (!dump) continue;
+            screeningFile.precision(6);
+            screeningFile.width(14);
+            screeningFile << std::scientific << x;
+            screeningFile.precision(6);
+            screeningFile.width(14);
+            screeningFile << std::scientific << phi;
+            screeningFile << std::endl;
+
+        if (isLast) break;
+    }
+    if (dump)
+        screeningFile.close();
+}
+
+void math_tools::ComputeExpansionCoefficients(std::vector<double> &coeffs){
+    int nCoeffs = (int) coeffs.size();
+
+    coeffs[0] = 1.0;
+    coeffs[1] = 9.0-std::sqrt(73.0);
+
+    for (int m = 2; m < nCoeffs; m++){
+        coeffs[m] = 1.0/(2.0*(m+8.0) - (m+1.0)*coeffs[1]);
+
+        double sum = 0.0;
+        for (int n = 1; n < m-2; n++){
+            sum += coeffs[m-n] *((n+1.)*coeffs[n+1] - 2.*(n+4.)*coeffs[n] + (n+7.)*coeffs[n-1]);
+        }
+        sum += coeffs[m-1]*((m+7.) - 2*(m+3.)*coeffs[1]) +
+            coeffs[m-2]*((m+6.)*coeffs[1]);
+        coeffs[m] = coeffs[m]*sum;
+    }
+}
